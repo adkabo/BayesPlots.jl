291478cbec8441936726554372db4f8ac8fce5bf
adkabo <adkabo>
2020-11-19 22:18:35 +0000

    Export draw

--- src/BayesPlots.jl
+++ src/BayesPlots.jl
@@ -4,4 +4,7 @@ include("plots.jl")
 
 export plot_histogram, plot_trace, plot_cummean, plot_partial_complete, plot_autocorrelation, plot_parameter_correlation, plot_coefficients
 
+# Reexport from AlgebraOfGraphics.
+export draw
+
 end
