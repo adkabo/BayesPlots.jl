e1cef3d3e21a505f1bddffe1f9ffe0662daede59
adkabo <adkabo>
2020-11-20 20:17:43 +0000

    Export plot_density

--- src/BayesPlots.jl
+++ src/BayesPlots.jl
@@ -2,7 +2,7 @@ module BayesPlots
 
 include("plots.jl")
 
-export plot_histogram, plot_trace, plot_cummean, plot_partial_complete, plot_autocorrelation, plot_parameter_correlation, plot_coefficients
+export plot_density, plot_histogram, plot_trace, plot_cummean, plot_partial_complete, plot_autocorrelation, plot_parameter_correlation, plot_coefficients
 
 # Reexport from AlgebraOfGraphics.
 export draw
